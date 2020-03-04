414e198fabcfa20a35fbda94e71b9bdfb0709f10
bobbidigi <bobbidigi34@gmail.com>
2020-03-04 16:40:17 -0500

    add update type working on updating location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { fetchVenues } from "./actions/venueActions";
 import { connect } from "react-redux";
@@ -6,16 +6,55 @@ import VenueList from './components/VenueList';
 
 function App(props) {
  console.log("APP.js",props)
+ const [newLocation, setNewLocation] = useState("Detroit");
+ const [newType, setNewType] = useState("tacos");
 
   useEffect(() => {
     // kick off our asyncronous action creator
-    props.fetchVenues("detroit", "tacos");
+    props.fetchVenues(newLocation, newType);
   }, []);
 
+  useEffect(()=>{
+    props.fetchVenues(newLocation, newType)
+  },[newType, newLocation])
 
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // setNewLocation("");
+    props.updateLocation(newLocation || props.location);
+    props.fetchTacoVenues(newLocation, newType)
+  };
+
+  //Ask Skylar if there is a way to 
+  const handleSelectChanges = e => {
+    setNewType(e.target.value)
+  }
+
+  const handleInputChanges = e => {
+    setNewLocation(e.target.value);
+  };
 
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="newLocation"
+            // placeholder={props.location} 
+            placeholder="city"
+            value={'' || newLocation}
+            onChange={handleInputChanges}
+          />
+          <select name="newType" onChange={handleSelectChanges}>
+            <option value={newType}>{newType}</option>
+            <option value='Restaurants'>Restaurants</option>
+            <option value="Events">Events</option>
+            <option value="Brewery">Brewery</option>
+          </select>
+          <button type="submit">Submit</button>
+        </form>
+
      <VenueList venues={props.venues}/>
     </div>
   );
