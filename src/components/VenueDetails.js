b709dbac85013142ce83a8f45ef752480f98b64b
bobbidigi <bobbidigi34@gmail.com>
2020-03-07 14:01:19 -0500

    add details, photo, image, location, phone

--- src/components/VenueDetails.js
+++ src/components/VenueDetails.js
@@ -2,11 +2,12 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import {useDispatch, useSelector} from 'react-redux'
 import {fetchDetails} from '../actions/venueActions'
+import '../App.css';
 
 
 
 export default function VenueDetails(props) {
-    console.log("details", props.history.location.state.venue.id)
+    // console.log("details", props.history.location.state.venue.id)
     const id = props.history.location.state.venue.id
     const dispatch = useDispatch()
     const state = useSelector(state => state)
@@ -16,21 +17,30 @@ export default function VenueDetails(props) {
     useEffect(()=>{
         dispatch(fetchDetails(id))
     },[])
+
     useEffect(()=>{
-        console.log("venueDetailState",state.details)
         setVenueDetails(state.details)
-        console.log("vendeets",venueDetails)
     },[state.details])
+    
+    useEffect(()=>{
+        console.log("deets", venueDetails)
+        // console.log(venueDetails.location.crossStreet)
+    },[venueDetails])
+
 
     return (
-        <div >
-            <p>{venueDetails.name}</p>
-            <p>{venueDetails.url}</p>
-            <div className='address'>
-         {/* {state.details.location.formattedAddress[0]} , {state.details.location.formattedAddress[1]} */}
-        </div>
-            {/* <p>{venueDetails.location.formattedAddress[0]}</p> */}
-        </div>
+            <div className="details-container">
+                {venueDetails.bestPhoto && <img className="hero" src={venueDetails.bestPhoto.prefix + '900x700' + venueDetails.bestPhoto.suffix}/>}
+                <div className="details-info">
+                    <h3>{venueDetails.name}</h3>
+                    <p>{venueDetails.url}</p>
+                    {venueDetails.location && <>
+                    {venueDetails.location.formattedAddress[0]}
+                    {venueDetails.location.crossStreet}</>}
+                    {venueDetails.contact && <p>{venueDetails.contact.formattedPhone}</p>}
+                </div>
+            </div>
+        
     )
 }
 
