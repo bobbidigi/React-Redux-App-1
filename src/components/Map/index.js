57fe0884b384d55dedfc0f62d6cdf4bcc00b41cd
bobbidigi <bobbidigi34@gmail.com>
2020-03-08 19:01:37 -0400

    update

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,29 +4,32 @@ import {useDispatch, useSelector} from 'react-redux'
 
 export default function Map() {
   const state = useSelector(state => state)
-  const [venueDetails, setVenueDetails] = useState(state.details)
+  const [venueLocation, setVenueLocation] = useState(state.details)
   console.log("map state", state.details.location)
 
 
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '50vh',
-    latitude: 37.7577,
-    longitude: -122.4376,
+    marginBottom: '15rem',
+   
     zoom: 11.5
   });
 
+  
+
   useEffect(()=>{
-    state.details.location && setViewport({
-      latitude: state.details.location.lat,
-      longitude: state.details.location.lng,
-      zoom: 12
-    })
+    state.details.location && setVenueLocation(state.details.location)
   },[])
 
   useEffect(()=>{
-    console.log("updated viewport", viewport)
-  },[viewport])
+    console.log("venueLocation",venueLocation)
+    venueLocation.lng && setViewport({
+      latitude: venueLocation.lat,
+      longitude: venueLocation.lng,
+    })
+    console.log("the updated viewport", viewport)
+  },[venueLocation])
 
   const REACT_APP_MAP_KEY = process.env.REACT_APP_MAP_KEY;
   return (
@@ -34,7 +37,7 @@ export default function Map() {
       mapboxApiAccessToken={REACT_APP_MAP_KEY}
       {...viewport}
       mapStyle="mapbox://styles/bobbidigi/ck7jfv8s83bvh1ipswl1hv564"
-      onViewportChange={viewport => {setViewport(viewport)}}
+      onViewportChange={setViewport}
     />
   );
 }
